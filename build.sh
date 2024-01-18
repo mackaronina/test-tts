a6748a8b57a1ecbcbb965f403ea25ba179081392
mackaronina <116784694+mackaronina@users.noreply.github.com>
2024-01-18 22:08:10 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html#docker
-set -e
-
-docker run \
-    --name api \
-    --rm \
-    -m 500m \
-    -p 80:80
